e0973512dcd8e616c1f24fd86de9af941913bbeb
johnnybinh <thebinhnguyen2703@gmail.com>
2024-09-15 23:04:33 +0700

    fix deployment #1

--- landingPage.js
+++ landingPage.js
@@ -1,22 +1,23 @@
-const characterSelection = document.getElementById('character-selection');
-const startButton = document.getElementById('start-button');
-let chosenCharacter = null; 
+const characterSelection = document.getElementById("character-selection");
+const startButton = document.getElementById("start-button");
+let chosenCharacter = null;
 
-
-characterSelection.addEventListener('click', (event) => {
+characterSelection.addEventListener("click", (event) => {
   const clickedElement = event.target;
-  if (clickedElement.classList.contains('character')) {
+  if (clickedElement.classList.contains("character")) {
     console.log(clickedElement);
-    chosenCharacter = clickedElement.dataset.character; 
-    characterSelection.querySelectorAll('.character').forEach(character => character.classList.remove('selected'));
-    clickedElement.classList.add('selected'); // Visually indicate selected character
+    chosenCharacter = clickedElement.dataset.character;
+    characterSelection
+      .querySelectorAll(".character")
+      .forEach((character) => character.classList.remove("selected"));
+    clickedElement.classList.add("selected"); // Visually indicate selected character
   }
 });
 
-startButton.addEventListener('click', () => {
+startButton.addEventListener("click", () => {
   if (chosenCharacter) {
     window.location.href = `index.html?character=${chosenCharacter}`;
   } else {
-    alert('Please choose a character!');
+    alert("Please choose a character!");
   }
 });
